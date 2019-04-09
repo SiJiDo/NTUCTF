e6401b80e4a8dbab162564e9eb917c14693d1ffb
SiJiDo <495105034@qq.com>
2019-04-09 12:53:52 +0800

    Add files via upload

---
+++ web_file/src/flag.php
@@ -0,0 +1,4 @@
+<html>
+	<head><meta charset="utf-8"></head>
+	<?php echo "你能找到flag吗? "?>
+</html>
---
+++ web_file/src/hack.php
@@ -0,0 +1,8 @@
+<?php
+	echo "flag就在这个源文件中  <h1>:)</h1>";
+	//NTUCTF{38c55423e123aca445982dfd897a552d}
+?>
+
+<html>
+	<head><meta charset="utf-8"></head>
+</html>
